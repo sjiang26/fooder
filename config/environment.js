5fffc5f3684ce5527d77a13226e500bbeeadf437
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-09-25 22:13:58 -0400

    Fix ember initialization

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'frontend',
+    modulePrefix: 'fooder',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
